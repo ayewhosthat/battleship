16523996f9b559c5a0d341859d7faa8156ebd774
ayewhosthat <alan.notiverson@gmail.com>
2024-07-22 16:44:32 -0400

    Add HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'main.js',
   },
+  plugins: 
+    [new HtmlWebpackPlugin({
+      title: 'Todo List',
+      template: './src/index.html',
+    }),
+    ],
   module: {
     rules: [
       {
